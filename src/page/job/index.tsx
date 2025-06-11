247ed2fbf9e9373114ec5e2a3d279fbdf2995f49
= <minhducdtr70@gmail.com>
2025-06-11 23:08:44 +0700

    update JobDetail

--- src/page/job/index.tsx
+++ src/page/job/index.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import JobCard from '@/components/client/card/job.card'
+import SearchClient from '@/components/client/search.client'
+import { Col, Divider, Row } from 'antd'
+import styles from "styles/client.module.scss"
 
 const ClientJobPage = () => {
     return (
-        <div>index</div>
+        <div className={styles["container"]} style={{ marginTop: 20 }}>
+            <Row gutter={[20, 20]}>
+                <Col span={24}>
+                    <SearchClient />
+                </Col>
+                <Divider />
+                <Col span={24}>
+                    <JobCard showPagination={true} />
+                </Col>
+            </Row>
+        </div>
     )
 }
 
